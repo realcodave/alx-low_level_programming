5e364c0a09495cdc5217b05cf06f5eeab1a40483
root <obienecollins@gmail.com>
2022-09-08 09:05:47 -0700

    adding c code to git

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int b;
 long int c;
 long long d;
 float e;
-printf("Size of a char: %c byte(s)",sizeof(a));
-printf("Size of an int: %d byte(s)",sizeof(b));
-printf("Size of a long int: %d byte(s)",sizeof(c));
-printf("Size of a long long: %L byte(s)",sizeof(d));
-printf("Size of a float: %d byte(s)",sizeof(e));
+printf("Size of a char: %c byte(s)\n",sizeof(a));
+printf("Size of an int: %d byte(s)\n",sizeof(b));
+printf("Size of a long int: %d byte(s)\n",sizeof(c));
+printf("Size of a long long: %L byte(s)\n",sizeof(d));
+printf("Size of a float: %d byte(s)\n",sizeof(e));
 return (0);
 }
