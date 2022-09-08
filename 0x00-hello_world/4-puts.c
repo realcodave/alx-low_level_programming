e96573cc722052bdab4b00b670ffcc45867bb4da
root <obienecollins@gmail.com>
2022-09-08 07:51:29 -0700

    adding c code to git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * this is tge main function that prints out strings of characters
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
