5a356947d8e872b12ebe132121098324f9ec1834
root <obienecollins@gmail.com>
2022-09-10 15:54:23 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter ='a';
+while (letter <= 'z')
+{
+putchar(letter++);
+}
+}
