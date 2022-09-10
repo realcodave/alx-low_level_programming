d087126d11e948758fefa6312eb11650be079cca
root <obienecollins@gmail.com>
2022-09-10 16:30:38 -0700

    letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.c>
+/**
+*main - Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter == 'e' || letters == 'q')
+{
+continue;
+}
+putchar(letter);
+letter++;
+};
+putchar('\n');
+}
