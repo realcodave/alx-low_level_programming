81715e6002c63b64600b76e51f91a018aac07bec
root <obienecollins@gmail.com>
2022-09-10 18:14:32 -0700

    letters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int b;
 long int c;
 long long d;
 float e;
-printf("Size of a char: %c byte(s)\n",sizeof(a));
+printf("Size of a char: %d byte(s)\n",sizeof(a));
 printf("Size of an int: %d byte(s)\n",sizeof(b));
 printf("Size of a long int: %d byte(s)\n",sizeof(c));
 printf("Size of a long long: %d byte(s)\n",sizeof(d));
