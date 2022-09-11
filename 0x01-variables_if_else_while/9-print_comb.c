a5699d62643ec5c2bf601558f12c416ec41326bb
root <obienecollins@gmail.com>
2022-09-10 17:48:45 -0700

    letters

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,16 @@ int main(void)
 int num = 0;
 while (num <= 9)
 {
-putchar(num + '0');
+putchar(num + '0 ');
+if (num == 9)
+{
+num++
+}
+else
+{
 putchar(',');
 num++;
+}
 }
 putchar('\n');
 return(0);
