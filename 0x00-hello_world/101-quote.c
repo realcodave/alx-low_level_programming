925caca34be611e7779c70817b57e33ee2b6abe3
root <obienecollins@gmail.com>
2022-09-10 18:31:01 -0700

    letters

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
