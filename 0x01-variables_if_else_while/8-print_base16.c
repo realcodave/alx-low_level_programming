64e1e17ae910449241493a8a769bdca63a8ed373
root <obienecollins@gmail.com>
2022-09-10 17:37:21 -0700

    letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num = 0;
+char letter = 'a';
+while (num <= '9')
+{
+putchar(num + '0');
+num ++;
+}
+while (letter <= 'f')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+return (0);
+}
