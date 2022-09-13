6a09c441166803ef4cfe3015dc84e4af6af74aa8
root <obienecollins@gmail.com>
2022-09-13 16:43:57 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - Entry point
+*
+*Description: 'the program's description'
+*
+*Return: Always 0 (Success)
+*/
+void alpha(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+}
+_putchar('\n')
+}
