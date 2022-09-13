d0ec6c435643d38c09a209dce05aed0096edb68b
root <obienecollins@gmail.com>
2022-09-13 16:58:05 -0700

    putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet – function to print abc
-* Description: This session 
-* Return: 0
+*print_alphabet – function to print abc
+*Description: This session
+*Return: 0
 */
 void print_alphabet(void)
 {
