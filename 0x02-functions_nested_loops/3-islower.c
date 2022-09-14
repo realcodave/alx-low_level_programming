87fa83af5fbc366e3749c055c791c1e3b9d14563
root <obienecollins@gmail.com>
2022-09-14 15:54:00 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include “main.h”
+/**
+* _islower – is alohabet lower
+* checking argument
+* Return: 0
+*/
+int _islower(char c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
