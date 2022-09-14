079f360bf35f4aed1e16a8c9c709d1d94db03d5e
root <obienecollins@gmail.com>
2022-09-14 15:42:45 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 char letter = 'a';
-int i;
+char i;
 
 for (i = 0; i <= 9; i++)
 {
@@ -16,7 +16,6 @@ while (letter <= 'z')
 _putchar(letter);
 letter++;
 }
-
 _putchar('\n');
 }
 }
