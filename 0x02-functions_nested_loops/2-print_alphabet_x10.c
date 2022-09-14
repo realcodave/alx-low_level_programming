66c32e614ba22bb6607d698d03dda31f92821973
root <obienecollins@gmail.com>
2022-09-14 15:18:35 -0700

    print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,16 @@
-h
+#include “main.h”
+/**
+* print_alphabet – function to print abc
+*
+* Return: 0
+*/
+Void print_alphabet(void)
+{
+char c = 'a';
+
+while (c <= 'z'; c++)
+{
+  _putchar(c);
+}
+_putchar(‘\n’);
+}
