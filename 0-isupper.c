7e05aa3c4903a2ac9be3ed09b36fba519ed357aa
root <obienecollins@gmail.com>
2022-09-15 16:15:41 -0700

    is upper

---
+++ 0-isupper.c
@@ -0,0 +1,14 @@
+/** 
+* _isupper - checks for uppercase.
+* @c: character to check.
+* Return: 1 if c is uppercase, otherwise return0. 
+*/
+int _isupper(int letter)
+{ 
+if (letter >= 'A' && letter <= 'Z')
+{
+return (1); 
+}
+else 
+return (0); 
+}
