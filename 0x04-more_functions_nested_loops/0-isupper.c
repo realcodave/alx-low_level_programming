ec8bd4f3d1c2f6698ca47c79800d27b29dd72558
root <obienecollins@gmail.com>
2022-09-15 16:24:23 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
-/** 
+/**
 * _isupper - checks for uppercase.
 * @c: character to check.
 * Return: 1 if c is uppercase, otherwise return0. 
 */
 int _isupper(int letter)
-{ 
+{
 if (letter >= 'A' && letter <= 'Z')
 {
-return (1); 
+return (1);
 }
-else 
-return (0); 
+else{
+return (0);
+}
 }
