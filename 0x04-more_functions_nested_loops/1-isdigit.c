546235d4f2eb41cff7357e1a622bb9a78f5d701b
root <obienecollins@gmail.com>
2022-09-15 17:13:41 -0700

    new

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase.
+* @num: character to check.
+* Return: 1 if num is uppercase, otherwise return 0
+*/
+int _isupper(int num)
+{
+if (num >= 0 && letter <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
