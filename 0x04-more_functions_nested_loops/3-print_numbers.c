44c4d3a57ec5d103523a39e1ec64142b049b0d92
root <obienecollins@gmail.com>
2022-09-18 15:49:28 -0700

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
 * _purchar - 0 - 9
 * Return: Always 0.
 */
-void print_numbers(void) 
-{ 
-int i = 0; 
-
-while (i <= 9) 
-{ 
-_putchar(i + '0'); 
-++i; 
-} 
-_putchar('\n'); 
+void print_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+{
+_putchar(i + '0');
+++i;
+}
+_putchar('\n');
 }
