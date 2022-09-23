06c6d93c03b67f63368d83e773e5849ffaf3d1ff
root <obienecollins@gmail.com>
2022-09-23 10:46:22 -0700

    prog

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
-* more_numbers - prints the range 0-14 ten times.
-* Return: nothing.
+*more_numbers - prints the range 0-14 ten times.
+*Return: nothing.
 */
+void more_numbers(void)
+{
+int i, j;
+i = j = 0;
 void more_numbers(void)
 {
 int i, j;
@@ -12,13 +16,12 @@ while (i < 10)
 while (j <= 14)
 {
 if (j >= 10)
-{
-_putchar(j / 10 + '0')
-};
+_putchar(j / 10 + '0');
 _putchar(j % 10 + '0');
 ++j;
 }
 _putchar('\n');
 j = 0;
 i++;
+}
 }
